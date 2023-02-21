44240566404744fd46642b643208a2dee55cd2d7
jasurrahmon1994 <jasurrahmon24@gmail.com>
2023-02-21 12:29:45 +0500

    first try

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,13 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> res = new ArrayList<>();
+        for (int i = 0; i < sourceList.size(); i++) {
+            if(i % 3 == 2){
+                res.add(sourceList.get(i));
+                res.add(sourceList.get(i));
+            }
+        }
+        return res;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,25 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+            sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int i = Integer.parseInt(a);
+        int j = Integer.parseInt(b);
+        int x = 5 * i * i + 3;
+        int y = 5 * j * j + 3;
+        if(x == y){
+            if(i > j){
+                return 1;
+            } else return -1;
+        } else if (x > y){
+            return 1;
+        } else {
+            return -1;
+        }
     }
 }
